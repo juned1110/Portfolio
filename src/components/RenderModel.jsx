9fd552e0a546b1d9cf103da71c9ad99ef0ebb2ce
juned1110 <junedking1110@gmail.com>
2024-07-15 14:29:04 +0530

    renderModel component created

---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RenderModel = () => {
+  return (
+    <React.Fragment>
+      <div>RenderModel</div>
+    </React.Fragment>
+  );
+};
+
+export default RenderModel;
