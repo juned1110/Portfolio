2a7bda5f4f26051750cf10792ab00ff688e3bbbd
juned1110 <junedking1110@gmail.com>
2024-07-15 15:48:21 +0530

    RenderModel component created

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+"use client";
+import React, { Suspense } from "react";
+import { Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
 
-const RenderModel = () => {
+const RenderModel = ({ children, className }) => {
   return (
     <React.Fragment>
-      <div>RenderModel</div>
+      <Canvas className={clsx("w-screen h-screen -z-10 relative", className)}>
+        <Suspense fallback={null}>{children}</Suspense>
+        <Environment preset="dawn" />
+      </Canvas>
     </React.Fragment>
   );
 };
