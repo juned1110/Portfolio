cc5c1fe0482f3e2dac4a99d21a348cee44ca4bc3
juned1110 <junedking1110@gmail.com>
2024-07-15 15:47:11 +0530

    render model and wizard 3d component added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import bg from "../../public/background/home-background.png";
+import RenderModel from "@/components/RenderModel";
+import Wizard from "@/components/models/Wizard";
 
 export default function Home() {
   return (
@@ -7,8 +9,16 @@ export default function Home() {
       <Image
         src={bg}
         alt="background-image"
+        fill
         className="w-full h-full object-cover object-center opacity-25"
       />
+
+      <div className="w-full h-screen">
+        {/* navigation and 3d component */}
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
+      </div>
     </main>
   );
 }
