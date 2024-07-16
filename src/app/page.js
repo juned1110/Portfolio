a4427091fa49b0a341ae590ea893c42e3d00e6fc
juned1110 <junedking1110@gmail.com>
2024-07-16 12:06:26 +0530

    Navigation component mounted on page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
 import Wizard from "@/components/models/Wizard";
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
 
       <div className="w-full h-screen">
         {/* navigation and 3d component */}
+        <Navigation />
         <RenderModel>
           <Wizard />
         </RenderModel>
