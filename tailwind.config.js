7433121cac2d724850bbbea2daaf739cc44072c6
juned1110 <junedking1110@gmail.com>
2024-07-16 14:22:05 +0530

    boxShadow, keyframes and animation added in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         muted: "rgb(var(--muted))",
         accent: "rgb(var(--accent))",
       },
+      boxShadow: {
+        "glass-inset": "inset 0 17px 5px -9px rgba(254,254,91, 0.05)",
+        "glass-sm": "5 5px 20px 0px rgba(254,254,91, 0.09)",
+      },
+      keyframes: {
+        "spin-reverse": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+        "spin-slow-reverse": "spin-reverse 40s linear infinite",
+      },
     },
   },
   plugins: [],
